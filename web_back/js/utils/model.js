d344de3ae97903b60c1e48f4e806788aa72c5fcc
zhao <1156956096@qq.comm>
2019-11-19 17:02:23 +0800

    用户退出

--- web_back/js/utils/model.js
+++ web_back/js/utils/model.js
@@ -3,18 +3,34 @@
 // user 是对象  login是方法
 var local = 'http://localhost:8000';
 var user = {
-    // 方法里面有三个形参  callback是回调函数
-    login:function (username,password,callback) {
-        $.post(local+"/admin/login",
-        // 参数
-            {
-                user_name:username,
-                password:password
-                },
-                function (res) {
-                   callback(res)
-                   
-                }
-        )
-    }
+        // 方法里面有三个形参  callback是回调函数
+        login:function (username,password,callback) {
+            $.post(local+"/admin/login",
+            // 参数
+                {
+                    user_name:username,
+                    password:password
+                    },
+                    
+                    function (res) {
+                        // 相当于把响应体一起传给callback
+                        callback(res)
+                    
+                    }
+            )
+        },
+        // 退出的方法
+        logout:function (callback) {
+            console.log(callback);
+            
+            $.post("http://localhost:8000/admin/logout",function(res){
+
+                 callback(res);
+                 console.log(res);
+                 
+             })
+        }
 }
+
+
+    

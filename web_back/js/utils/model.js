9ca8ec95dc4592aec87e8def652447ef1ba65dee
zhao <1156956096@qq.comm>
2019-11-19 16:15:22 +0800

    用户登录优化

--- web_back/js/utils/model.js
+++ web_back/js/utils/model.js
@@ -1,10 +1,11 @@
 // 模块化思想
 // 创建一个对象 里面放方法
 // user 是对象  login是方法
+var local = 'http://localhost:8000';
 var user = {
     // 方法里面有三个形参  callback是回调函数
     login:function (username,password,callback) {
-        $.post("http://localhost:8000/admin/login",
+        $.post(local+"/admin/login",
         // 参数
             {
                 user_name:username,

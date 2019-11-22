27e5c19b68103733305d15fba004851792538567
zhao <1156956096@qq.comm>
2019-11-22 20:15:31 +0800

    分页完成

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -1,19 +1,27 @@
 var article = {
-    show:function (callback) {
-        $.get(PROT.article_show,function (res) {
+    show:function (obj,callback) {
+        $.get(PROT.article_show,{page:obj.page},function (res) {
             console.log(res);
             callback(res)
         })
     },
 
-    publish:function (callback) {
-        $.post(PROT.article_publish,function (res) {
+    // publish:function (callback) {
+    //     $.post(PROT.article_publish,
+    //         {data:fd, processData:false,contentType:false},
+    //         function (res) {
+    //         callback(res)
+    //     })
+    // },
+
+    del:function (id,callback) {
+        $.get(PROT.article_del,{id:id},function (res) {
             callback(res)
         })
     },
 
-    del:function (id,callback) {
-        $.get(PROT.article_del,{id:id},function (res) {
+    edit:function (callback) {
+        $.post(PROT.article_edit,function (res) {
             callback(res)
         })
     }
